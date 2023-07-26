a4bb7d03266622ec2899b893d18515fd21b7b091
Shweta <shweta@famark.com>
2023-07-26 14:42:35 +0530

    Added Logic for Update and Delete

--- Program.cs
+++ Program.cs
@@ -13,13 +13,13 @@ internal class Program
         Console.Write("Enter User Name: ");
         string userName = Console.ReadLine();
         Console.Write("Enter Password: ");
-        string passord = Console.ReadLine();
+        string password = Console.ReadLine();
 
         Credential credential = new Credential
         {
             DomainName = domainName,
             UserName = userName,
-            Password = passord
+            Password = password
         };
 
         string credString = SerializeToJson<Credential>(credential);
@@ -61,7 +61,7 @@ internal class Program
             }
             else
             {
-                Console.WriteLine("Cannot Create Record!");
+                Console.WriteLine("Cannot Create Record! " + api.ErrorMessage);
                 Console.Write("Do you want to try again? (Y or N): ");
             }
 
@@ -85,6 +85,62 @@ internal class Program
             foreach (Business_Contact contact in contacts)
             {
                 Console.WriteLine("{0}| {1} | {2} | {3} | {4} | {5}", (++i).ToString("000"), contact.FirstName, contact.LastName, contact.Phone, contact.Email, contact.Business_ContactId);
+            }
+        }
+        // Performing Update action getting Information from user
+        Console.WriteLine("Do you wish to update record?  (Y or N):");
+        ans = Console.ReadKey();
+        if (ans.KeyChar == 'Y' || ans.KeyChar == 'y')
+        {
+            Console.WriteLine();
+            Business_Contact contact = new Business_Contact();
+            Console.Write("Enter Record Id: ");
+            contact.Business_ContactId = Console.ReadLine();
+            Console.Write("Enter First Name: ");
+            contact.FirstName = Console.ReadLine();
+            Console.Write("Enter Last Name: ");
+            contact.LastName = Console.ReadLine();
+            Console.Write("Enter Phone: ");
+            contact.Phone = Console.ReadLine();
+            Console.Write("Enter Email: ");
+            contact.Email = Console.ReadLine();
+
+            string contactData = SerializeToJson(contact);
+            responseData = await api.PostData("Business_Contact/UpdateRecord", contactData, sessionId);
+
+            if (!string.IsNullOrEmpty(responseData))
+            {
+                string contactId = DeserializeFromJson<string>(responseData);
+                Console.WriteLine("Record Updated: " + contactId);
+            }
+            else
+            {
+                Console.WriteLine("Cannot Update Record! " + api.ErrorMessage);
+            }
+        }
+
+        Console.WriteLine();
+        // Performing Delete action getting Information from user
+        Console.WriteLine("Do you wish to delete record? (Y or N): ");
+        ans = Console.ReadKey();
+        if (ans.KeyChar == 'Y' || ans.KeyChar == 'y')
+        {
+            Console.WriteLine();
+            Business_Contact contact = new Business_Contact();
+            Console.Write("Enter Record Id: ");
+            contact.Business_ContactId = Console.ReadLine();
+
+            string contactData = SerializeToJson(contact);
+            responseData = await api.PostData("Business_Contact/DeleteRecord", contactData, sessionId);
+
+            if (!string.IsNullOrEmpty(responseData))
+            {
+                string contactId = DeserializeFromJson<string>(responseData);
+                Console.WriteLine("Record Deleted: " + contactId);
+            }
+            else
+            {
+                Console.WriteLine("Cannot Delete Record! " + api.ErrorMessage);
             }
         }
     }
